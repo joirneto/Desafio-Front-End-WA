b1a9a3ec0da2f26b2885a8fbc5345a15331c9f70
joirneto <joirneto@gmail.com>
2021-11-17 18:59:50 -0300

    Update questions.js

--- pages/questions.js
+++ pages/questions.js
@@ -13,6 +13,8 @@ const Questions = () =>{
   useEffect(async () => {
     try {
       const response = await getQuestions(num)
+      
+     
       setQuestions(response)
     } catch (error) {
       console.log(error)
@@ -23,12 +25,13 @@ const Questions = () =>{
 
     questions.map((question, index) => {
       let aux = []
-      aux.push(question.correct_answer)
+      aux.push(question.correct_answer.replace(/&quot;/g,' ' ).replace("&#039;",' ').replace("&eacute;", 'e'))
+      
       for(let i=0; i<question.incorrect_answers.length;++i){
-        aux.push(question.incorrect_answers[i])
+        aux.push(question.incorrect_answers[i].replace(/&quot;/g,' ' ).replace("&#039;",' ').replace("&eacute;", 'e'))
       }
       const aux1 = {
-        question: question.question,
+        question: question.question.replace(/&quot;/g,' ' ).replace(/&#039;/,' ').replace("&eacute;", 'e'),
         answers: aux
       }
 
@@ -39,20 +42,29 @@ const Questions = () =>{
     console.log('TRE=',questionsX)
     
     questions.forEach(question =>{
-      console.log("Q=",question.question.replace(/&quot;/g, '"'))
+      console.log("Q=",question.question)
     })
   }
-  console.log("Q=",questions)
-
+ 
+ 
   return(
     <>
     <div className="bg-indigo-600 p-2">
       <div className=" w-full container mx-auto my-4 flex flex-col justify-center bg-white leading-none ${props.textColor} rounded-lg p-2 shadow text-teal text-sm">
     <Buttons></Buttons>
-    {questions.map(item =>{
+    {questionsX.map((item, index) =>{
           return(
             <>
-            <label className=''>{item.question.replace(/&quot;/g, '"')}</label>
+            <label className='' >{`Question ${index+1}`} {item.question}</label>
+            {item.answers.map(ans =>{
+                return(
+                  <div className="flex items-center mb-4">
+                  <input name={item.question} className="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300" type='radio' value={ans} />
+                  <label className="text-sm font-medium text-gray-900 ml-2 block">{ans} <br/> </label>
+                  </div>
+                )
+            })}
+              
             
             </>
           )
