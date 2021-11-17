e03568310207afe25320754fed2216e55f6d2839
joirneto <joirneto@gmail.com>
2021-11-17 00:25:28 -0300

    rotas

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Link from 'next/link'
+
+
 
 const Index = () =>{
+  const [num, setNum] = useState('');
+
+  const onChange = evt =>{
+    const value = evt.target.value
+    setNum(value)
+  }
+  
+
   return(
      <section className="bg-indigo-600 p-2">
        <div className="w-full container mx-auto my-4 flex flex-col justify-center bg-white leading-none ${props.textColor} rounded-full p-2 shadow text-teal text-sm">
@@ -13,10 +24,12 @@ const Index = () =>{
         </span>
         </div>
         <div className ="flex flex-col items-center">
-        <input className ="w-2/12 m-2 p-4 text-lg text-center bg-gray-100 rounded-full border" type="text" placeholder="NUMBER OF QUESTIONS"/>
-        <button type="button" className="w-2/12 my-2 p-4 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
-        REQUEST
-      </button>
+        <input className ="w-2/12 m-2 p-4 text-lg text-center bg-gray-100 rounded-full border" type="text" name='num' placeholder="NUMBER OF QUESTIONS" value={num} onChange={onChange}/>
+        <Link href ={ { pathname: '/questions', query: { num: num } }}>
+      <a className="w-2/12 my-2 p-4 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg">
+      Quero Indicar/Cadastrar!
+      </a>
+    </Link>
         </div>
     </div>
      </section>
