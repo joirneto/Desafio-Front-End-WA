a5b5dabed8c034d8e92aba88a40966c21ba0ef92
joirneto <joirneto@gmail.com>
2021-11-17 16:54:30 -0300

    Update questions.js

--- pages/questions.js
+++ pages/questions.js
@@ -8,6 +8,7 @@ const Questions = () =>{
   const [questions, setQuestions] = useState([]);
   const router = useRouter()
   const num = router.query.num;
+  const questionsX = [];
 
   useEffect(async () => {
     try {
@@ -18,14 +19,42 @@ const Questions = () =>{
     }
   }, []);
 
+  if(questions.length !== 0){
+
+    questions.map((question, index) => {
+      let aux = []
+      aux.push(question.correct_answer)
+      for(let i=0; i<question.incorrect_answers.length;++i){
+        aux.push(question.incorrect_answers[i])
+      }
+      const aux1 = {
+        question: question.question,
+        answers: aux
+      }
+
+      questionsX.push(aux1)
+      
+    });
+
+    console.log('TRE=',questionsX)
+    
+    questions.forEach(question =>{
+      console.log("Q=",question.question.replace(/&quot;/g, '"'))
+    })
+  }
+  console.log("Q=",questions)
+
   return(
     <>
     <div className="bg-indigo-600 p-2">
       <div className=" w-full container mx-auto my-4 flex flex-col justify-center bg-white leading-none ${props.textColor} rounded-lg p-2 shadow text-teal text-sm">
     <Buttons></Buttons>
-    {questions.map(ob =>{
+    {questions.map(item =>{
           return(
-            <p>{ob.question}</p>
+            <>
+            <label className=''>{item.question.replace(/&quot;/g, '"')}</label>
+            
+            </>
           )
         })}
     </div>
